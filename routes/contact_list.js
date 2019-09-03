9367d619f45a7e75d1dfb7d61cd4fa7512834e0c
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-09-03 16:25:07 -0400

    Init contact_list router

---
+++ routes/contact_list.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const contact_listRouter = express.Router();
+const Contact_ListService = require('../services/contact_list');
+const uuidv1 = require('uuid/v1');
+
+// POST - CREATE 
+contact_listRouter.post('/', (req, res) => {
+  const {host, contact} = req.body;
+  let uuid = uuidv1();
+  Contact_ListService.create(uuid, host, contact)
+    .then(data => {
+      res.status(201);
+      res.send(`Confirmed contact add: ${data}`);
+    })
+    .catch(err => {
+      res.status(400);
+      res.send({"Message":err})
+    })
+});
+
+// GET - READ 
+contact_listRouter.getContacts('/:uuid', (req, res) => {
+  const {uuid} = req.params;
+  Contact_ListService.read(uuid)
+    .then(data => {
+      res.status(200);
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(400);
+      res.send({"Message":err})
+    })
+});
+
+// DELETE - DELETE
+contact_listRouter.delete('/:uuid',(req,res)=>{
+  const {uuid} = req.params;
+  Contact_ListService.delete(uuid)
+  .then(()=>{
+    res.status(201)
+    res.send(`Contact has been removed from your list.`)
+  })
+  .catch(err =>{
+    res.status(400);
+    res.send({"Message":err})
+  })
+})
+
+
+
+module.exports = contact_listRouter;
