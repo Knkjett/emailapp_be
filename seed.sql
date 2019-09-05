1ddd533641ca402cf34fc695e3655e5043d9671c
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-09-05 04:20:22 -0400

    Added not null ot contact_list seed

--- seed.sql
+++ seed.sql
@@ -23,8 +23,8 @@ CREATE TABLE history(
 
 CREATE TABLE contact_list(
   uuid TEXT NOT NULL,
-  host TEXT REFERENCES users(uuid),
-  contact TEXT REFERENCES users(uuid),
+  host TEXT REFERENCES users(uuid) NOT NULL,
+  contact TEXT REFERENCES users(uuid) NOT NULL,
   category TEXT DEFAULT NULL,
   PRIMARY KEY (uuid)
 );
